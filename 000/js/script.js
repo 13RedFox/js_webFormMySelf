de425af5bd6b1756dd14e101b3916fb1433f7a08
Serhii <sergei181989@gmail.com>
2020-04-29 20:47:11 +0300

    Learning JS courses block #3

--- 000/js/script.js
+++ 000/js/script.js
@@ -16,7 +16,7 @@ if (carAge < 5) {
 
 //Funtion
 
-let carName = "BMW",
+/* let carName = "BMW",
   carYear = 2012,
   personYear = 1990;
 
@@ -26,13 +26,82 @@ function calculateAge(year) {
   return result;
 }
 
-function checkAngLongAge(year){
-  if (calculateAge(year) < 10) {
-    console.log("Возраст меньше 10 лет");
+function checkAngLongAge(year, name, compareTo) {
+  if (calculateAge(year) < compareTo) {
+    console.log("Возраст " + name + " меньше " + compareTo + " лет");
   } else {
-    console.log("Возраст больше 10 лет");
+    console.log("Возраст " + name + " больше " + compareTo + " лет");
   }
 }
 
-checkAngLongAge(carYear);
-checkAngLongAge(personYear);
+checkAngLongAge(carYear, "Машины", 8);
+checkAngLongAge(personYear, "Человека", 30); */
+
+/* let sayHelloTo = function (name) {
+  console.log("Привет, " + name);
+};
+
+sayHelloTo("Elena"); */
+
+// STRING
+
+/* let str1 = "Hello world!",
+  personName = "Victor",
+  message = "Человека зовут '" + personName + "'",
+  message2 = 'Человека зовут "' + personName + '"',
+  newMessage = "Hello world!!!";
+
+console.log(message);
+console.log(message2);
+console.log(newMessage.length);
+console.log(newMessage.toUpperCase());
+console.log(newMessage.toLowerCase());
+console.log(newMessage.charAt(0));
+console.log(newMessage.indexOf("world"));
+console.log(newMessage.substr(1, 3));
+console.log(newMessage.substring(3));
+ */
+
+// Masive
+/* 
+let car = ["BMW", "Mazda", "Toyota", "VAZ"];
+
+console.log(car);
+console.log(car[1]);
+console.log(car.length);
+
+car.push('Audi');
+console.log(car); 
+*/
+
+// Object
+/* 
+let person = {
+  name: "Petrik",
+  year: 1990,
+  family: ["Elena", "Igor"],
+  car: {
+    year: 2010,
+    model: "Ford",
+  },
+  calculateAge: function () {
+    this.age = 2018 - this.year;
+  },
+};
+
+console.log(person);
+console.log(person.name);
+console.log(person.year);
+
+person.calculateAge();
+ */
+
+// Циклы
+
+/* let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    console.log(numbers[i]);
+  }
+} */
---
+++ 001/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/* let div = document.getElementById("playground"),
+  p = document.getElementsByClassName("text"),
+  h1 = document.getElementsByTagName("h1");
+
+console.log(div);
+console.log(p);
+console.log(h1); */
+
+/* let div = document.querySelector('#playground'),
+p = document.querySelectorAll('.text'),
+h1 = document.querySelector('h1')
+
+console.log(div);
+console.log(p);
+console.log(h1);
+ */
